f15680ad72fe97809662303b6ddd1887f197b923
Alyssa <92120729+real-housewives-of-python@users.noreply.github.com>
2023-02-24 14:11:42 +0100

    Update logreg.py

--- logreg.py
+++ logreg.py
@@ -167,7 +167,7 @@ def main(argv=None):
                 
     trainingfile = argv[1] #Training file
     inputfile = argv[2] #Development or Test file
-    modelname = argv[3] #'logreg' or 'cnb' or 'svm'
+    modelname = argv[3] #'logreg' 
 
     
     feature_values, labels = extract_features_and_gold_labels(trainfile, selected_features=selected_feature)
