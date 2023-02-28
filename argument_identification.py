dd430e70df577a613abf97b245d21068c9c13eee
sezentuvay <35296484+sezentuvay@users.noreply.github.com>
2023-02-28 02:24:02 +0100

    Update argument_identification.py

--- argument_identification.py
+++ argument_identification.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 def make_gold_binary(inputfile, outputfile):
+    #take the inputfile (which is the output of feature_extraction.py), and changes the numbers in the last column to zeros and ones
+    #this is then saved in a new output file
     data = pd.read_csv(inputfile, encoding = 'utf-8', sep = '\t')
     binary_gold = []
     for gold in data['Gold']:
@@ -21,5 +23,6 @@ def main():
 if __name__ == '__main__':
     main()
 
+#this script only needs to be run ones and all three files will be created. In the data-folder, you will need to create an output folder. 
 
 
